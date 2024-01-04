1fd8a4c48834d98c015aff2be80f3ec22ea89349
Agustin Jaralampidis <ajaralampidis@gmail.com>
2024-01-04 17:17:42 -0300

    feat: husky tailwind eslint and prettier

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const BASE_URL = import.meta.env.BASE_URL;
+
+export const SITE_TITLE = 'Agustin Jaralampidis Dev';
+export const SITE_DESCRIPTION =
+	'Welcome to my website! I like development, design, 3D and much more';
+export const SITE_IMAGE_URL = `${BASE_URL}/assets/cube/cubePoster.png`;
